1252bfe306900eff62cad3794c7cdcc2e84ea895
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-11-09 15:43:32 +0000

    [Add]top,about,login,sign_upページの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  
+  root :to => 'users/homes#top'
+  get 'about' => 'users/homes#about'
+  
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip:[:registrations, :passwords],controllers:{
+    sessions: "admin/sessions"
+  }
+  
+  # 顧客用
+  # URL /users/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "users/registrations",
+    sessions: 'users/sessions'
+  }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
