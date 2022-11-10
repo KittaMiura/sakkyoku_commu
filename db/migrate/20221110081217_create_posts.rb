dd2920ecb9972673c833e29ee29e7c4238aa5ea5
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-11-10 08:37:15 +0000

    [Add]投稿モデルの追加

---
+++ db/migrate/20221110081217_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+
+      t.string :title, null: false
+      t.text :introduction, null: false
+      t.integer :user_id
+      t.integer :post_tag_id
+
+      t.timestamps
+    end
+  end
+end
