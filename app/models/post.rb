21b32d2ed60d71425efcd995a7d4acbdbba75a34
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-11-11 15:48:15 +0000

    [Add]タグ機能のモデル追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+  has_many :post_tags,dependent: :destroy
+  has_many :tags,through: :post_tags
 
   #　↓画像が無い場合は「no_image」の画像を使うようにする
   def get_image
