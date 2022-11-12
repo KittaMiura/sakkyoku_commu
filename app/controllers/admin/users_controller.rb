d088b62bf5f52ac5ebb73dd3ab81f5b178fc127c
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-11-12 22:53:51 +0000

    [Add]adminの会員管理機能実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,8 +3,24 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
-  
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    # updateの構文：モデルのインスタンス.update(更新データ)
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "会員情報が更新されました。"
+    else
+      render "edit"
+    end
+  end
 
   private
 
