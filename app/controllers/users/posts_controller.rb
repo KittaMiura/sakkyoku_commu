c39f0b490091fd6250e0aabbd26de938147339c5
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-11-12 03:58:30 +0000

    mainへ移動するためコミット

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -11,9 +11,15 @@ class Users::PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.user_id = current_user.id
-    @post.save
-    redirect_to posts_path
+    @post.user_id=current_user.id
+    # 受け取った値を,で区切って配列にする
+    tag_list=params[:post][:name].split(',')
+    if @post.save
+      @post.save_tag(tag_list)
+      redirect_to posts_path(@post),notice:'投稿完了しました:)'
+    else
+      render:new
+    end
   end
 
   def show
