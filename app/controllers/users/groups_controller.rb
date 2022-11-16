92538b3c52af491fb8cfd20fe0f03de2be26a2d8
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-11-16 16:27:27 +0000

    [Add]グループメンバー機能の追加

--- app/controllers/users/groups_controller.rb
+++ app/controllers/users/groups_controller.rb
@@ -11,6 +11,12 @@ class Users::GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def join
+    @group = Group.find(params[:group_id])
+    @group.users << current_user
+    redirect_to groups_path
+  end
+
   def new
     @group = Group.new
   end
@@ -18,6 +24,7 @@ class Users::GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
+    @group.users << current_user
     if @group.save
       redirect_to groups_path
     else
@@ -34,6 +41,13 @@ class Users::GroupsController < ApplicationController
     else
       render "edit"
     end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    #current_userは、@group.usersから消されるという記述。
+    @group.users.delete(current_user)
+    redirect_to groups_path
   end
 
   private
