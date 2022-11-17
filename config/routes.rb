4fc744755442c9311139e241063ce70c0c0f3539
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-11-17 19:58:03 +0000

    [Add]メール機能実装、まだメルアド設定はなし

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ Rails.application.routes.draw do
     resources :groups do
       get "join" => "groups#join"
       delete "all_destroy" => 'groups#all_destroy'
+      get "new/mail" => "groups#new_mail"
+      get "send/mail" => "groups#send_mail"
     end
   end
 
